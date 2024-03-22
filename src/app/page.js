ddfd910c65fb2cd222bbfe605ae8af79b40c1ec1
Denise Stark <starkwalker88@gmail.com>
2024-03-22 16:32:13 -0700

    finished homepage

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import {allBlogs} from "../../.contentlayer/generated";
 import FeaturePosts from "../components/Home/FeaturePosts";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
+import RecentPosts from "../components/Home/RecentPosts";
 
 export default function Home() {
   console.log("allBlogs", allBlogs);
@@ -9,6 +10,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturePosts blogs={allBlogs} />
+      <RecentPosts blogs={allBlogs} />
     </main>
   )
 }
