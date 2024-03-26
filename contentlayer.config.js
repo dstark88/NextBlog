b5162ccd76f55d5991588f98c2b90438619f6134
Denise Stark <starkwalker88@gmail.com>
2024-03-26 13:15:10 -0700

    blog linking and rendering one blog

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,4 +1,10 @@
 import { makeSource, defineDocumentType } from "@contentlayer/source-files";
+import readingTime from "reading-time";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+import GithubSlugger from "github-slugger"
 
 const Blog = defineDocumentType(() => ({
   name: "Blog",
@@ -21,9 +27,7 @@ const Blog = defineDocumentType(() => ({
       type: "string",
       required: true,
     },
-    image: {
-      type: "image"
-     },
+    image: { type: "image" },
     isPublished: {
       type: "boolean",
       default: true,
@@ -42,11 +46,43 @@ const Blog = defineDocumentType(() => ({
       type: "string",
       resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
     },
+    readingTime: {
+      type: "json",
+      resolve: (doc) => readingTime(doc.body.raw)
+    },
+    toc:{
+      type: "json",
+      resolve: async (doc) => {
+
+        const regulrExp = /\n(?<flag>#{1,6})\s+(?<content>.+)/g;
+        const slugger = new GithubSlugger();
+        const headings = Array.from(doc.body.raw.matchAll(regulrExp)).map(({groups}) => {
+          const flag = groups?.flag;
+          const content = groups?.content;
+
+          return {
+            level: flag?.length == 1 ? "one" : flag?.length == 2 ? "two" : "three",
+            text: content,
+            slug: content ? slugger.slug(content) : undefined
+          }
+
+        })
+
+
+        return headings;
+      }
+    }
   },
 }));
+
+const codeOptions = {
+  theme: 'github-dark',
+  grid: false,
+}
 
 export default makeSource({
   /* options */
   contentDirPath: "content",
   documentTypes: [Blog],
+  mdx: { remarkPlugins: [remarkGfm], rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {behavior: "append"}], [rehypePrettyCode, codeOptions] ] }
 });
